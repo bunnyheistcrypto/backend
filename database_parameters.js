1de47a0a39162561f8ebefd38d1dd991d1030cc0
Vinicius Teodoro <32147785+vinniteodoro@users.noreply.github.com>
2022-05-01 01:39:12 -0300

    Refatoração dos arquivos
    Simplificação dos arquivos para um melhor entendimento e manutenção mais fácil e eficiente

---
+++ database_parameters.js
@@ -0,0 +1,23 @@
+/*module.exports = {
+    dialect: 'mysql',
+    host: '10.129.76.12',
+    username: 'dargos1',
+    password: 'BunnyHeist0+',
+    database: 'bunnyheist_test',
+    define: {
+        timestamps: true,
+        underscored: true
+    }
+}*/
+
+module.exports = {
+    dialect: 'mysql',
+    host: 'localhost',
+    username: 'root',
+    password: 'bunnyheist',
+    database: 'teste',
+    define: {
+        timestamps: true,
+        underscored: true
+    }
+}
--- index.js
+++ index.js
@@ -1,37 +1,22 @@
-const { Sequelize, DataTypes } = require('sequelize');
-const dbConfig = require('./database');
+(async () => {
+    const db = require('./database');
+    const dbEstructures = require('./database_estructures');
+    await db.sync();
+})();
 
-const db = new Sequelize(dbConfig.database, dbConfig.username, dbConfig.password, {
-    host: dbConfig.host,
-    dialect: dbConfig.dialect,
-    logging: console.log
-});
-module.exports = db;
-
+/*
 db.authenticate().then(() => {
     console.log('Sucesso na conexão com o DB');
 }).catch((error) => {
     console.error('Falha na conexão:', error);
 });
 
-const User = db.define('User', {
-    id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true
-    },
-    username: {
-        type: DataTypes.STRING,
-        allowNull: false
-    }
-});
-db.sync;
-
 const result = User.findAll();
-//console.log(JSON.stringify(result, null, 2));
+console.log(JSON.stringify(result, null, 2));
 
-/*try {
+try {
     const vini = User.create({ id:2, username:'vinietbasilio' });
 } catch(error) {
     console.log(error);
-}*/
-
+}
+*/
